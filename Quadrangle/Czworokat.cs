209029aaf9fff1d15c97aedbda87164e0aa47cab
ChAgata <agata.chrin.g23@gmail.com>
2019-08-04 17:53:44 +0100

    Add project files.

---
+++ Quadrangle/Czworokat.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quadrangle
+{
+    public abstract class Czworokat
+    {
+        public int a;
+        public int b;
+        public int c;
+        public int d;
+        public virtual void Wypisz(int a, int b, int c, int d)
+        {
+            Console.WriteLine("Długości boków: {0}, {1}. {2}, {3}.", a, b, c, d);
+        }
+        public abstract int Pole(int a, int b, int c, int d);
+        public Czworokat(int a, int b, int c, int d)
+        {
+            this.a = a;
+            this.b = b;
+            this.c = c;
+            this.d = d;
+        }
+        public Czworokat(int a, int b)
+        {
+            this.a = a;
+            this.b = b;
+            this.c = a;
+            this.d = b;
+        }
+
+    }
+
+}
---
+++ Quadrangle/Prostokat.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quadrangle
+{
+    public class Prostokat : Czworokat
+    {
+        public Prostokat(int argA, int argB):base (a, b)
+        {
+
+            this.a = argA;
+            this.b = argB;
+            this.c = argA;
+            this.d = argB;
+        }
+
+        public override int Pole(int a, int b, int c, int d)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
